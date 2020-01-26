bfd7308fe0ac6d9ac6405b4f45d083a45b94da79
Prashant Chamoli <prashantchamoli.165@gmail.com>
2020-01-26 10:55:22 +0530

    Create db.js
    Main function to connect to DB

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true });
+    console.log("database connected...");
+  } catch (err) {
+    console.log("error message >>> " + err.message);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
