0492e450247154040407b44cb5c4e5df0882479c
Prashant Chamoli <prashantchamoli.165@gmail.com>
2020-01-27 14:16:32 +0530

    Added API routes for various resources

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 const connectDB = require("./config/db");
 
 connectDB();
+app.use(express.json({ extended: false }));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+
 app.get("/", (req, res) => res.send("API hit"));
 
 const PORT = process.env.PORT || 5000;
