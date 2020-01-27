464947006e4638d85d98a136a0e4531882b00706
Prashant Chamoli <prashantchamoli.165@gmail.com>
2020-01-27 16:06:47 +0530

    Completed register user API. JWT implementation remaining.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
-
+const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const User = require("../../models/User");
 router.post(
   "/",
   [
@@ -13,12 +15,47 @@ router.post(
       min: 6
     })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     } else {
-      return res.status(201).json(req.body);
+      try {
+        const { name, email, password } = req.body;
+        let user = await User.findOne({ email });
+
+        if (user)
+          return res
+            .status(400)
+            .json({ errors: [{ msg: "User already present in system" }] });
+
+        const avatar = gravatar.url(email, {
+          s: "200",
+          r: "pg",
+          d: "mm"
+        });
+        const salt = await bcrypt.genSalt(10);
+
+        user = new User({
+          name,
+          email,
+          avatar,
+          password
+        });
+
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+        user.password = "";
+
+        //else do something
+        // User.insertOne(req.);
+        return res.status(201).json(user);
+      } catch (err) {
+        console.error(err.message);
+        return res.status(500).send("Internal server error");
+      }
+
+      // return res.status(201).json(req.body);
     }
   }
 );
