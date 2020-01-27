7baee3bda17d34cf1ba9a52730039455c7b61de7
Prashant Chamoli <prashantchamoli.165@gmail.com>
2020-01-27 16:05:50 +0530

    Fixed some warnings

--- config/db.js
+++ config/db.js
@@ -4,7 +4,11 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true });
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
     console.log("database connected...");
   } catch (err) {
     console.log("error message >>> " + err.message);
