ce736e17e976aab8d37493af0399827aa879738b
Prashant Chamoli <prashantchamoli.165@gmail.com>
2020-01-29 00:44:55 +0530

    Added JWT authorization to register user API.

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
+const config = require("config");
+
 router.post(
   "/",
   [
@@ -44,12 +47,28 @@ router.post(
         });
 
         user.password = await bcrypt.hash(password, salt);
+        console.log(`id is >> ${user.id}`);
         await user.save();
         user.password = "";
 
         //else do something
         // User.insertOne(req.);
-        return res.status(201).json(user);
+        // add jwt to the response
+
+        const payload = {
+          user: {
+            id: user.id
+          }
+        };
+        jwt.sign(
+          payload,
+          config.get("jwtSecret"),
+          { expiresIn: 36000 },
+          (err, token) => {
+            if (err) throw err;
+            return res.status(201).json({ token });
+          }
+        );
       } catch (err) {
         console.error(err.message);
         return res.status(500).send("Internal server error");
