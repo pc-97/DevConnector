4e5543b0767bd7e65440c5886c24af0371d170b6
Prashant Chamoli <prashantchamoli.165@gmail.com>
2020-01-29 00:50:09 +0530

    Added JWT secret key

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,6 +10,8 @@ function checkJWT(req, res, next) {
   try {
     const decoded = jwt.verify(token, config.get("jwtSecret"));
     req.user = decoded.user;
+    next();
+  } catch (err) {
     res.status(401).json({ msg: "token is not valid" });
   }
 }
