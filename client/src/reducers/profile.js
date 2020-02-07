79ccedbf76b2ab095b1541598c81d845f5182e52
Prashant Chamoli <prashantchamoli.165@gmail.com>
2020-02-07 22:56:00 +0530

    Reducer

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,47 @@
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+  ACCOUNT_DELETED
+} from "../actions/types";
+
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {}
+};
+
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_PROFILE:
+    case UPDATE_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false
+      };
+
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: payload
+      };
+
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        loading: false,
+        repos: []
+      };
+
+    default:
+      return state;
+  }
+}
